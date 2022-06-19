0cdaa18096a153eaaa2adae4406a16f9d4c3a6c1
Enggar <enggarprathitaj@gmail.com>
2022-06-19 22:31:41 +0700

    add login page

--- EnergyWEB/EnergyWEB/ClientApp/src/components/eText.js
+++ EnergyWEB/EnergyWEB/ClientApp/src/components/eText.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { FormGroup, Input, Label } from "reactstrap";
 
 const Etext = (props) => {
 
@@ -21,12 +22,19 @@ const Etext = (props) => {
         return res;
     }
 
+    
     return (
-        <div className={"form-group"}>
-            <label htmlFor={props.id}>{props.label}</label>
-            <input type={setType(props.type)} onChange={props.onChange} className={"form-control"} id={props.id} value={setData(props.value,props.type)}></input>
-            {props.hintMessage != "" ? <small id={props.id + "Help"} className="form-text text-muted">{props.hintMessage}</small>:""}
+        <div>
+            <FormGroup>
+                <Label for={props.id}>{props.label}</Label>
+                <Input type={props.type} onChange={props.onChange} placeholder={props.placeholder} name={props.name}/>
+            </FormGroup>
         </div>
+        // <div className={"form-group"}>
+        //     <label htmlFor={props.id}>{props.label}</label>
+        //     <input type={setType(props.type)} onChange={props.onChange} className={"form-control"} id={props.id} value={setData(props.value,props.type)}></input>
+        //     {props.hintMessage != "" ? <small id={props.id + "Help"} className="form-text text-muted">{props.hintMessage}</small>:""}
+        // </div>
     )
 }
 
--- EnergyWEB/EnergyWEB/ClientApp/src/container/Login.js
+++ EnergyWEB/EnergyWEB/ClientApp/src/container/Login.js
@@ -1,22 +1,70 @@
 import React, {Component} from "react";
-import {Card, CardBody, Container, Row, Col} from "reactstrap";
+import {Card, CardBody, Container, Row, Col, CardTitle, Spinner} from "reactstrap";
+import Etext from "../components/eText";
+import Ebutton from "../components/eButton";
 
 class Login extends Component {
+
+    constructor(props){
+        super(props);
+        this.state = {
+            username: "",
+            password : "",
+            isDone: true
+        }
+    }    
+
+    doLogin = async () => {
+        this.setState({isDone : false})
+        // alert("username : " + this.state.username + " password : " + this.state.password);
+        setTimeout(() => {
+            this.setState({isDone : true})
+        },5000)
+    }
+
     render() {
         return (
             <div>
                 <Container>
                     <Row>
+                        <br/><br/><br/><br/>
+                        &nbsp;
+                    </Row>
+                    <Row>
+                        <br/>
+                        &nbsp;
+                    </Row>
+                    <Row>
+                        <br/>
+                        &nbsp;
+                    </Row>
+                    <Row>
+                        <Col md="3"></Col>
                         <Col md="6">
-                                
-                        </Col>
-                        <Col md="6">
-                                <Card>
+                                <Card
+                                    body 
+                                    className={"shadow-sm p-3 mb-5 bg-white rounded"}>
                                     <CardBody>
-                                        <h1>tes</h1>
+                                      <Container>
+                                        <Row>
+                                            <Col sm="2"></Col>
+                                            <Col sm="8">
+                                                <center><b><h3>EnergyWeb</h3></b></center>
+                                            </Col>
+                                            <Col sm="2"></Col>
+                                        </Row>
+                                        <Row>
+                                            <Col sm="12">
+                                                <Etext value={this.state.username} onChange={(e)=> {this.setState({username : e.target.value})}} id="username" label="Username" type="text"/>
+                                                <Etext value={this.state.password} onChange={(e)=> {this.setState({password : e.target.value})}} id="password" label="Password" type="password"/>
+                                                {this.state.isDone?<Ebutton event={this.doLogin} displayText="Login" block={true} size="large"/>:<center><Spinner color="primary"></Spinner></center>}
+                                            </Col>
+                                        </Row>
+                                     </Container>  
                                     </CardBody>
                                 </Card>        
                         </Col>
+                        <Col md="3"></Col>
                     </Row>
                 </Container>
                 
